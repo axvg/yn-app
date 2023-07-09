aeca3e5c5d0c50b476da32743b301cd439e3f9cf
axvg <91773368+axvg@users.noreply.github.com>
2023-07-09 02:28:50 -0500

    avatar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:yes_no_app/config/theme/app_theme.dart';
+import 'package:yes_no_app/presentation/screens/chat/chat_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,13 +18,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Material App Bar'),
         ),
-        body: Center(
-          child: FilledButton.tonal(
-              onPressed: () {
-                print('clicked');
-              },
-              child: const Text('Click me!')),
-        ),
+        body: ChatScreen(),
       ),
     );
   }
---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('bot: '),
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://robohash.org/2'),
+          ),
+        ),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 10,
+        ),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return Text('Index: $index');
+              },
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
